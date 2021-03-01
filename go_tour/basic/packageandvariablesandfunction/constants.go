93291243a9ede73a19c8946b85ac69e10d256f31
张立雨 <josiahzhang@dazwang.com>
2021-03-01 14:58:17 +0800

    goTour update

---
+++ go_tour/basic/packageandvariablesandfunction/constants.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+const Pi = 3.14
+
+func main() {
+
+	const World = "workd"
+
+	fmt.Println("hello ", World)
+	fmt.Println("hello ", Pi)
+
+	const Truth = false
+	fmt.Println("Go rules?", Truth)
+
+}
